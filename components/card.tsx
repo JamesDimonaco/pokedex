74c2b4091b0be5116e5cd87317c31cd4a183c89b
James Dimonaco <dimonaco.james@gmail.com>
2022-10-24 23:30:27 +0100

    clean up some duplicate code

--- components/card.tsx
+++ components/card.tsx
@@ -124,12 +124,9 @@ const Card = ({ searchedData, getPokemon, getPokemonDetails }: Props) => {
             setOpen(true)
           }, [setPokemonFullDetails, setOpen])
 
-
-          if (data.pages.length === pageIndex + 1 && page.pokemon.length === index + 1){
-
             return (
               <li
-                ref={lastPokemonRef}
+                ref={data.pages.length === pageIndex + 1 && page.pokemon.length === index + 1 ? lastPokemonRef : null}
                 key={pokemon.url}
                 className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
               >
@@ -168,55 +165,6 @@ const Card = ({ searchedData, getPokemon, getPokemonDetails }: Props) => {
                 }
               </li>
             )
-
-          } else {
-            return (
-              <li
-  
-                key={pokemon.url}
-                className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
-              >
-                <div className="flex flex-1 flex-col p-8">
-                  <h3 className="mt-6 text-sm font-medium text-gray-900">{pokemonName}</h3>
-                  <dl className="mt-1 flex flex-grow flex-col justify-between">
-                    <dd className="mt-3">
-                    </dd>
-                  </dl>
-                </div>
-                <div>
-                  <div className="-mt-px flex divide-x divide-gray-200">
-                    <div className="flex w-0 flex-1">
-                      <a
-                        onClick={() => manageEvolution(pokemon.name)}
-                        className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
-                      >
-                        <span className="mx-3">Evolution</span>
-                      </a>
-                    </div>
-                    <div className="-ml-px flex w-0 flex-1">
-                      <a href={`/${pokemon.name}`}
-                        className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
-                      >
-                        <span className="ml-x">Details</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                {
-                  open &&
-  
-                  <Modal open={open} setOpen={setOpen} pokemonFullDetails={pokemonFullDetails} />
-  
-                }
-              </li>
-            )
-
-          }
-
-
-
-
-
 
         }))}
       {isFetching && <Loader />}
@@ -224,7 +172,6 @@ const Card = ({ searchedData, getPokemon, getPokemonDetails }: Props) => {
 
 
   )
-
 
   return (
     <div>{
